c39a6a77c95a7b8c9248cf4884769dc8c3987b11
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2020-04-08 11:55:04 +0100

    Add brackets to print statements

--- fuse.py
+++ fuse.py
@@ -638,7 +638,7 @@ class Operations(object):
 
 class LoggingMixIn:
     def __call__(self, op, path, *args):
-        print '->', op, path, repr(args)
+        print('->', op, path, repr(args))
         ret = '[Unhandled Exception]'
         try:
             ret = getattr(self, op)(path, *args)
@@ -647,4 +647,4 @@ class LoggingMixIn:
             ret = str(e)
             raise
         finally:
-            print '<-', op, repr(ret)
+            print('<-', op, repr(ret))
